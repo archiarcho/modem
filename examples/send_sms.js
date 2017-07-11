d2f4c0cbb38cc17de0bc73615c06be1b91643c07
Nick Cephas <cephas.nick@archenis.com>
2017-07-11 11:17:18 +0000

    Envoi/reception sms/accusé de reception, ussd,

--- examples/send_sms.js
+++ examples/send_sms.js
@@ -5,23 +5,3 @@ function err(message) {
   process.exit();
 }
 
-var device   = process.argv[2];
-var receiver = process.argv[3];
-var text     = process.argv[4];
-
-if(!device || !receiver || !text) err();
-
-var modem = require('../index.js').Modem();
-modem.open('/dev/ttyUSB0', function() {
-  modem.sms({
-    receiver:receiver,
-    text:text,
-    encoding:'7bit'
-  }, function(err, sent_ids) {
-    console.log('>>', arguments);
-    if(err)
-      console.log('Error sending sms:', err);
-    else
-      console.log('Message sent successfully, here are reference ids:', sent_ids.join(','));
-  });
-});
--- index.js
+++ index.js
@@ -1,7 +1,4 @@
-//-------- By archi archo Ok
-  let a = '';
 return module.exports = {
   Modem : require('./lib/modem.js'),
   Ussd_Session : require('./lib/ussd_session.js')
-  //---------- Mine
 }
